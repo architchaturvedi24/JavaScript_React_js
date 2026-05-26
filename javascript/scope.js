ea8af5a63b53507b1e27fc416cc1d9967e315b9d
architchaturvedi24 <architchaturvedi24@gmail.com>
2026-05-27 00:04:58 +0530

    let var const scope

---
+++ javascript/scope.js
@@ -0,0 +1,20 @@
+console.log("Hello World")
+console.log("Hello World")
+
+// document.write("Hello world")
+
+var num = 5;
+console.log(typeof(num))
+
+//Java Script Variables
+
+/* all variables have 
+Name
+Type
+value or "null"
+to declare a variable
+var variableName
+let x = 10
+x = 20     //Redeclaration is possible in let, But re-initialization is not allowed 
+//re-initialization is allowed in var
+*/
